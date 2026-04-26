4f182fccfc5b1c7da03d3cd2c316b123100c470e
parussel <48210244+parussel@users.noreply.github.com>
2026-04-26 09:30:11 +0200

    Update 70_TibberConnect.pm

--- FHEM/70_TibberConnect.pm
+++ FHEM/70_TibberConnect.pm
@@ -1,1 +1,40 @@
+###############################################################################
+# $Id: 70_TibberConnect.pm 00001 2026-04-26 05:30:00Z Franky.P $
+###############################################################################
+#  70_TibberConnect.pm
+#
+#  (c) Programmer 2026-... Frank Parussel
+#  https://forum.fhem.de/index.php?action=profile;u=23804
+#
+#  This module provides a comprehensive integration for Tibber, including
+#  real-time price monitoring, automated device control for negative prices,
+#  load management, and long-term price trend analysis.
+#
+#  Copyright notice
+#
+#  This script is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  The GNU General Public License can be found at
+#  http://www.gnu.org/copyleft/gpl.html.
+#  A copy is found in the text file GPL.txt and important notices to the license
+#  from the author is found in LICENSE.txt distributed with these scripts.
+#
+#  This script is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  This copyright notice MUST APPEAR in all copies of the script!
+#
+##############################################################################
+#
+# define <name> TibberConnect <TOKEN> <HOME_ID>
+#
+##############################################################################
+
+
+##############################################################################
 
